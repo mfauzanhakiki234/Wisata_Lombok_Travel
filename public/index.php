8145f0c65a7ed92eb4cc5c0a45741dbb65fd9db4
mfauzan hakiki123 <mfauzanhakiki@saintek.unipdu.ac.id>
2026-06-20 15:12:58 +0700

    fix: full configuration of public index for laravel 12 on vercel

--- public/index.php
+++ public/index.php
@@ -1,11 +1,29 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+// --- CONFIG VERCEL SERVERLESS STORAGE ---
+putenv('APP_STORAGE=/tmp/storage');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('CACHE_STORE=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+
+$required_folders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/bootstrap/cache'
+];
+foreach ($required_folders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0755, true);
+    }
+}
+putenv('BOOTSTRAP_CACHE_PATH=/tmp/storage/bootstrap/cache');
+// --- END CONFIG VERCEL ---
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Determine if the application is under maintenance...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
@@ -14,7 +32,11 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Force Laravel to use Vercel temporary storage path
+$app->useStoragePath('/tmp/storage');
+
+$app->handleRequest(
+    Illuminate\Http\Request::capture()
+);
