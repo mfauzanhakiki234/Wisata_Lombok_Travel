ea0df584dfaccef9d43e21ed6354360f9699e4e8
mfauzan hakiki123 <mfauzanhakiki@saintek.unipdu.ac.id>
2026-06-20 17:47:16 +0700

    fix: ganti runtime ke vercel-laravel dan paksa register view provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,26 +4,21 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
+
+// PAKSA PENDAFTARAN VIEW ENGINE UNTUK VERCEL
+$app->register(Illuminate\View\ViewServiceProvider::class);
+
+return $app;
